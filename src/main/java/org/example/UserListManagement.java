1010da554d458dca97a975692f984901a0cd56a6
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-12 17:46:36 +0200

    Create UserManagement interface and implement it in UserListManagement class

--- src/main/java/org/example/UserListManagement.java
+++ src/main/java/org/example/UserListManagement.java
@@ -1,12 +1,35 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-public class UserListManagement {
+public class UserListManagement implements UserManagement{
     ArrayList<User> userList = new ArrayList<>();
 
 
     public void addUser(User user) {
         this.userList.add(user);
     }
+
+    @Override
+    public ArrayList<User> showAllUsers() {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findUserById(String userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void removeUser(String userId) {
+
+    }
+
+    @Override
+    public void changeUserRole(String userId, Role userRole) {
+
+    }
+
+
 }
